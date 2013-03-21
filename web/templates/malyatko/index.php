d6c2c059b85342451faf87841a5494bf2e1ea075
Admin <ira@ubuntu.ubuntu-domain>
2013-03-21 22:07:53 +0200

    menu

--- web/templates/malyatko/index.php
+++ web/templates/malyatko/index.php
@@ -7,6 +7,7 @@ $menu = & JSite::getMenu();
 
 $catid = JRequest::getInt('catid');
 
+
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
